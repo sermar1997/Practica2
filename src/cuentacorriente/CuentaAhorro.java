9017c4584612c002e249a81b262af6d91bce5ba6
Sergio <Sergio@Sergio>
2017-05-08 12:54:17 +0200

    Practica 2 github

--- src/cuentacorriente/CuentaAhorro.java
+++ src/cuentacorriente/CuentaAhorro.java
@@ -9,6 +9,17 @@ package cuentacorriente;
  *
  * @author Sergio
  */
-public class CuentaAhorro {
-    
+public class CuentaAhorro extends CuentaCorriente{
+    double interes;
+    public CuentaAhorro(String nombre, String numCuenta, double saldo, double interes){
+        super(nombre, numCuenta, saldo, 0);
+        this.interes=interes;
+    }
+    public CuentaAhorro(String nombre, String numCuenta, double interes){
+        super(nombre, numCuenta);
+        this.interes=interes;
+    }
+    public double getInteres(){
+        return this.interes;
+    }
 }
